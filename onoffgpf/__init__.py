788745be8efdcadfd6279ab96a77844f467bdeeb
Kyle Heuton <kheuton@gmail.com>
2023-05-18 00:21:12 -0400

    Add batch import

--- onoffgpf/__init__.py
+++ onoffgpf/__init__.py
@@ -1,5 +1,6 @@
 from .OnOffSVGP import OnOffSVGP, OnOffSVGPMC, OnOffSVGPPoiMC
+from .OnOffSVGPBatch import OnOffSVGPBatch
 from .OnOffLikelihood import OnOffLikelihood
 from .OnOffPoissonLikelihood import OnOffPoissonLikelihood
 
-__all__ = ['OnOffSVGP','OnOffSVGPMC','OnOffSVGPPoiMC','OnOffLikelihood', 'OnOffPoissonLikelihood']
+__all__ = ['OnOffSVGP','OnOffSVGPMC','OnOffSVGPPoiMC','OnOffSVGPBatch','OnOffLikelihood', 'OnOffPoissonLikelihood']
